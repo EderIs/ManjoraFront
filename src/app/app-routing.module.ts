7129f65ca7c895ab777eae746da06a81c44790b6
Eder Israel Basurto Andrade <bederisrael@gmail.com>
2020-12-28 19:13:00 -0600

    Creación de Métodos Angular Usuario

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DetalleUsuarioComponent } from './usuario/detalle-usuario.component';
+import { EditarUsuarioComponent } from './usuario/editar-usuario.component';
+import { ListaUsuarioComponent } from './usuario/lista-usuario.component';
+import { NuevoUsuarioComponent } from './usuario/nuevo-usuario.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'listar', component: ListaUsuarioComponent},
+  {path: 'detalle/:id', component: DetalleUsuarioComponent},
+  {path: 'nuevo', component: NuevoUsuarioComponent},
+  {path: 'editar/:id', component: EditarUsuarioComponent},
+  {path: '**', redirectTo:'listar', pathMatch: 'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
