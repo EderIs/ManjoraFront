73d2bd2e8f0fb9c3b6f48ceb3a0730a252ed81db
Alejandro Rodriguez <alejandro.rodriguezh1879@gmail.com>
2021-01-20 13:40:55 -0600

    Empleados modificado
    Agregar horas y direccion

--- src/app/models/empleado.ts
+++ src/app/models/empleado.ts
@@ -1,4 +1,6 @@
+import { Contacto } from "./contacto";
 import { Departamento } from "./departamento";
+import { HoraLaboral } from "./horaLaboral";
 import { Puesto } from "./puesto";
 import { Usuario } from "./usuario";
 
@@ -7,12 +9,12 @@ export class Empleado {
     fotografia: ByteLengthChunk;
     nombreEmpleado: String;
     idCategoria: number;
-    //direccionTrabajo: Contactos;
+    direccionTrabajo: Contacto;
     idPuesto: Puesto;
     tituloTrabajo: String;
     idResponsable: Empleado;
     idMonitor: Empleado;
-    //horasLaborales: HorasLaborales;
+    horasLaborales: HoraLaboral;
     //idCuentaBancaria: CuentaBancaria;
     direccionPrivada: String;
     contactoEmergencia: String;
@@ -30,18 +32,20 @@ export class Empleado {
     nota: String;
     estado: boolean;
 
-    constructor(fotografia: ByteLengthChunk, nombreEmpleado: String, idCategoria: number, idPuesto: Puesto,
-        tituloTrabajo: String, idResponsable: Empleado, idMonitor: Empleado, direccionPrivada: String,
+    constructor(fotografia: ByteLengthChunk, nombreEmpleado: String, idCategoria: number, direccionTrabajo: Contacto, idPuesto: Puesto,
+        tituloTrabajo: String, idResponsable: Empleado, idMonitor: Empleado, horasLaborales: HoraLaboral, direccionPrivada: String,
         contactoEmergencia: String, telefonoEmergencia: String, kmCasaTrabajo: number, Sexo: String,
         estadoCivil: String, numeroHijos: number, fechaNacimiento: Date, lugarNacimiento: String, nivelCertificado: String,
         escuela: String, notaAdicional: String, idUsuario: Usuario, nota: String, estado: boolean){
         this.fotografia = fotografia;
         this.nombreEmpleado = nombreEmpleado;
         this.idCategoria = idCategoria;
+        this.direccionTrabajo = direccionTrabajo;
         this.idPuesto = idPuesto;
         this.tituloTrabajo = tituloTrabajo;
         this.idResponsable = idResponsable;
         this.idMonitor = idMonitor;
+        this.horasLaborales = horasLaborales;
         this.direccionPrivada = direccionPrivada;
         this.contactoEmergencia = contactoEmergencia;
         this,telefonoEmergencia = telefonoEmergencia;
