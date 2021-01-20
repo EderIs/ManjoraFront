d2c01cc9afcc9cb964822714b81f076cc8a4efdc
Sebastian <villagrans131799@gmail.com>
2021-01-20 13:42:39 -0600

    Actualizado.
    version mas reciente.

--- src/app/models/estado.ts
+++ src/app/models/estado.ts
@@ -6,6 +6,7 @@ export class Estado {
     codigo:number;
     pais: Pais;
 
+    
     constructor(nombreEstado: string,codigo: number, pais: Pais){
         this.nombreEstado=nombreEstado;
         this.codigo=codigo;
